39f8e62666809b3000ad93ba3bbd6503bdb097a3
Ahmadahsana <ahmadahsanataqwim@gmail.com>
2023-02-17 18:00:00 +0700

    ok

--- src/components/images.js
+++ src/components/images.js
@@ -1,7 +1,20 @@
 import React, { useEffect, useRef, useState } from "react"
 import { Container } from "react-bootstrap"
+import axios from "axios"
+
 
 const Images = (props) => {
+    const [datas, setdatas] = useState([])
+
+    useEffect(() => {
+        axios({
+            method: "GET",
+            url: `${process.env.REACT_APP_BASEURL}/photos`
+        }).then((hasil) => setdatas(hasil.data))
+    }, [])
+
+    console.log(datas[0]);
+
     return (
         <React.Fragment>
             <Container className="mt-3">
